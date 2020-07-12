b03d4bfa43b240a0b7fcc0849ad11375b8252f59
Evseev-dev <jeny3d@mail.ru>
2020-07-12 19:31:55 +1000

    add-slider-section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
 $(function(){
-  
+
+  $('.rate-star').rateYo({
+    rating: 4,
+    starWidth: "14px",
+    spacing: "4px",
+    readOnly: true
+  });
+
+  $('.products-weekly__slider').slick({
+    infinite: true,
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img class="slick-img" src="images/icons/chevron-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img class="slick-img" src="images/icons/chevron-right.svg" alt=""></button>'
+  });
+
+
+  $('.products-weekly__item').css(
+    'display',''
+  );
+
+
+
 });
